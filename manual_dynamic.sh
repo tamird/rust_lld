e6f2c9f29005b0300fcc4e7bac91826e46167895
Tamir Duberstein <tamird@gmail.com>
2015-07-20 17:31:59 -0400

    Move flavor to beginning

--- manual_dynamic.sh
+++ manual_dynamic.sh
@@ -6,6 +6,6 @@ rustc --emit obj $FILE.rs -o target/$FILE.o
 
 STD=$(find $RUSTLIB -name libstd-*.dylib | head -n1 | sed s,$RUSTLIB/lib,, | sed s,\.dylib$,,)
 
-$LLVM/lld -demangle -dynamic -arch x86_64 -o target/hello_world -L$RUSTLIB target/hello_world.o -force_load $RUSTLIB/libmorestack.a -dead_strip -l$STD -lSystem -lpthread -lc -lm -rpath @loader_path/../../rust-build/x86_64-apple-darwin/stage2/lib/rustlib/x86_64-apple-darwin/lib -rpath /usr/local/lib/rustlib/x86_64-apple-darwin/lib -flavor darwin -lcompiler-rt
+$LLVM/lld -flavor darwin -demangle -dynamic -arch x86_64 -o target/hello_world -L$RUSTLIB target/hello_world.o -force_load $RUSTLIB/libmorestack.a -dead_strip -l$STD -lSystem -lpthread -lc -lm -rpath @loader_path/../../rust-build/x86_64-apple-darwin/stage2/lib/rustlib/x86_64-apple-darwin/lib -rpath /usr/local/lib/rustlib/x86_64-apple-darwin/lib -lcompiler-rt
 
 DYLD_LIBRARY_PATH=$RUSTLIB target/$FILE
--- manual_static.sh
+++ manual_static.sh
@@ -4,6 +4,6 @@ set -eu
 
 rustc --emit obj $FILE.rs -o target/$FILE.o
 
-$LLVM/lld -demangle -dynamic -arch x86_64 -o target/hello_world -L$RUSTLIB target/hello_world.o -force_load $RUSTLIB/libmorestack.a -dead_strip $RUSTLIB/lib{core,std,alloc,collections,rustc_unicode}*.rlib -lSystem -lpthread -lc -lm -lcompiler-rt -flavor darwin
+$LLVM/lld -flavor darwin -demangle -dynamic -arch x86_64 -o target/hello_world -L$RUSTLIB target/hello_world.o -force_load $RUSTLIB/libmorestack.a -dead_strip $RUSTLIB/lib{core,std,alloc,collections,rustc_unicode}*.rlib -lSystem -lpthread -lc -lm -lcompiler-rt
 
 target/$FILE
